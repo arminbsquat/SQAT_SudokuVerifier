e2020d592811fdf6f676b6ffb72adc988ce13c19
somename <someemail>
2016-09-16 16:38:18 +0300

    besouro automatic message

--- tests/SudokuVerifierTest.java
+++ tests/SudokuVerifierTest.java
@@ -28,14 +28,14 @@ public class SudokuVerifierTest {
 	@Test 
 	public void test_noReplicaInEveryRow() {
 		SudokuVerifier sV = new SudokuVerifier(); 
-		String candidateSolutionString = "123456784412345678891234567789123456678912345567891234456789123345678912234567891";
+		String candidateSolutionString = "417369825632158947958724316825437169791586432346912758289643571573291684164875293";
 		assertThat(sV.verify(candidateSolutionString), anyOf(is(-1), is(-3)));
 	}
 	
 	@Test
 	public void test_noReplicaInEveryColumn() {
 		SudokuVerifier sV = new SudokuVerifier(); 
-		String candidateSolutionString = "123456789912345678891234567789123456678912345567891234456789123345678912234567891";
+		String candidateSolutionString = "417369825632158947958724316825437169791586432346912758289643571573291684164875293";
 		assertThat(sV.verify(candidateSolutionString), anyOf(is(-1), is(-3), is(-4)));
 	}
 	
