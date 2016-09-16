28a8417e538ada8987425643d0a0348358511d6f
somename <someemail>
2016-09-16 17:08:09 +0300

    besouro automatic message

--- tests/SudokuVerifierTest.java
+++ tests/SudokuVerifierTest.java
@@ -29,31 +29,31 @@ public class SudokuVerifierTest {
 	public void test_noReplicaInEveryRow_noneFound() {
 		SudokuVerifier sV = new SudokuVerifier(); 
 		String candidateSolutionString = "417369825632158947958724316825437169791586432346912758289643571573291684164875293";
-		assertEquals(sV.verify(candidateSolutionString), 0);
-		//assertThat(sV.verify(candidateSolutionString), anyOf(is(-1), is(-3)));
+		//assertEquals(sV.verify(candidateSolutionString), 0);
+		assertThat(sV.verify(candidateSolutionString), anyOf(is(-1), is(-3)));
 	}
 	
 	@Test
 	public void test_noReplicaInEveryColumn_noneFound() {
 		SudokuVerifier sV = new SudokuVerifier(); 
 		String candidateSolutionString = "417369825632158947958724316825437169791586432346912758289643571573291684164875293";
-		assertEquals(sV.verify(candidateSolutionString), 0);
-		//assertThat(sV.verify(candidateSolutionString), anyOf(is(-1), is(-3), is(-4)));
+		//assertEquals(sV.verify(candidateSolutionString), 0);
+		assertThat(sV.verify(candidateSolutionString), anyOf(is(-1), is(-3), is(-4)));
 	}
 	
 	@Test
 	public void test_noReplicaInEveryColumn_replicaFound() {
 		SudokuVerifier sV = new SudokuVerifier(); 
 		String candidateSolutionString = "123456789912345678891234567789123456678912345567891234456789123345678912234567891";
-		assertEquals(sV.verify(candidateSolutionString), 0);
-		//assertThat(sV.verify(candidateSolutionString), anyOf(is(-1), is(-3), is(-4)));
+		//assertEquals(sV.verify(candidateSolutionString), 0);
+		assertThat(sV.verify(candidateSolutionString), anyOf(is(-1), is(-3), is(-4)));
 	}
 	
 	@Test 
 	public void test_noReplicaInEveryRow_replicaFound() {
 		SudokuVerifier sV = new SudokuVerifier(); 
 		String candidateSolutionString = "123456789912345678891234567789123456678912345567891234456789123345678912234567891";
-		assertEquals(sV.verify(candidateSolutionString), 0);
+		//assertEquals(sV.verify(candidateSolutionString), 0);
 		assertThat(sV.verify(candidateSolutionString), anyOf(is(-1), is(-3)));
 	}
 	
