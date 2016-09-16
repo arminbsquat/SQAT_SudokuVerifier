b310773431d4672c4577b2fddef8d925dda13622
somename <someemail>
2016-09-16 16:41:30 +0300

    besouro automatic message

--- tests/SudokuVerifierTest.java
+++ tests/SudokuVerifierTest.java
@@ -33,6 +33,11 @@ public class SudokuVerifierTest {
 	}
 	
 	@Test
+	public void test_noReplicaInSingleRow() {
+		
+	}
+	
+	@Test
 	public void test_noReplicaInEveryColumn() {
 		SudokuVerifier sV = new SudokuVerifier(); 
 		String candidateSolutionString = "417369825632158947958724316825437169791586432346912758289643571573291684164875293";
