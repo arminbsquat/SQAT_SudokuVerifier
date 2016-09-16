c31d642d95388762c468ae60e321ed6c5060b052
somename <someemail>
2016-09-16 16:13:34 +0300

    besouro automatic message

--- tests/SudokuVerifierTest.java
+++ tests/SudokuVerifierTest.java
@@ -29,7 +29,7 @@ public class SudokuVerifierTest {
 	public void test_noReplicaInSingleRow() {
 		SudokuVerifier sV = new SudokuVerifier(); 
 		String candidateSolutionString = "417369825632158947958724316825437169791586432346912758289643571573291684164875293";
-		assertThat(sV.verify(candidateSolutionString), anyOf((-1), (-3)));
+		assertThat(sV.verify(candidateSolutionString), anyOf(is(-1), is(-3)));
 	}
 	
 	
