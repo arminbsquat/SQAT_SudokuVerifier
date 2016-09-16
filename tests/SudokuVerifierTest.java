0b65db21779464894043cc80fa57802bc22d12f0
somename <someemail>
2016-09-16 16:24:24 +0300

    besouro automatic message

--- tests/SudokuVerifierTest.java
+++ tests/SudokuVerifierTest.java
@@ -1,5 +1,5 @@
 import static org.junit.Assert.*;
-import static org.hamrest.CoreMatchers.*;
+import static org.hamcrest.CoreMatchers.*;
 import org.junit.Test;
 
 public class SudokuVerifierTest {
@@ -28,8 +28,8 @@ public class SudokuVerifierTest {
 	@Test 
 	public void test_noReplicaInSingleRow() {
 		SudokuVerifier sV = new SudokuVerifier(); 
-		String candidateSolutionString = "417369825632158947958724316825437169791586432346912758289643571573291684164875293";
-		assertThat(sV.verify(candidateSolutionString), anyOf(is(-1), is(-3)));
+		String rightCandidateSolutionString = "417369825632158947958724316825437169791586432346912758289643571573291684164875293";
+		assertThat(sV.verify(rightCandidateSolutionString), anyOf(is(-1), is(-3)));
 	}
 	
 	
