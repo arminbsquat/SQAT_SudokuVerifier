961698fcd978f842f6798fbee330d001015da946
somename <someemail>
2016-09-16 15:22:40 +0300

    besouro automatic message

--- src/SudokuVerifier.java
+++ src/SudokuVerifier.java
@@ -15,10 +15,10 @@ public class SudokuVerifier {
 		
 		for(int i = 0; i < candidateSolutionIntArray.length; i++) {
 			if(!(candidateSolutionIntArray[i] > 0)) {
-				
+				return false;
 			} else
 			{
-				return false;
+				
 			}
 		}
 		
