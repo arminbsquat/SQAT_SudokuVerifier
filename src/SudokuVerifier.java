e4ebf98e11f282d41e6ba80ce6abba99db58305b
somename <someemail>
2016-09-16 15:54:22 +0300

    besouro automatic message

--- src/SudokuVerifier.java
+++ src/SudokuVerifier.java
@@ -24,7 +24,7 @@ public class SudokuVerifier {
 	public void noReplicaInSingleRow() {
 		int[] singleRow = new int[9]; 
 		
-		// add next 9 elements to singleRow
+		// build a row to test
 		for(int i = 0; i < 9; i++) {
 			singleRow[i] = candidateSolutionIntArray[offset + i];
 		}
