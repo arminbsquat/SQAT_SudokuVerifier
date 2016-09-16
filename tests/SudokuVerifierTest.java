f002ac5e9c466e3277cada2d31847e856cb80ec9
somename <someemail>
2016-09-16 15:09:52 +0300

    besouro automatic message

--- tests/SudokuVerifierTest.java
+++ tests/SudokuVerifierTest.java
@@ -12,6 +12,7 @@ public class SudokuVerifierTest {
 		fail("Not yet implemented");
 	}
 	
+	@Test
 	public void test_convertToIntArray() {
 		SudokuVerifier sV = new SudokuVerifier(); 
 		String candidateSolutionString = "417369825632158947958724316825437169791586432346912758289643571573291684164875293";
